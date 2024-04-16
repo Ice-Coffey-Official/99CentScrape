f0f4251720527661a86abc0937d2f8630ddf78be
Ice-Coffey-Official <167157587+Ice-Coffey-Official@users.noreply.github.com>
2024-04-16 04:49:38 -0700

    Update Config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import random
 class config:
      
     def __init__(self):
-        self.__states__ = ['ca'] #defaults to ['ca','az','nv','tx']
+        self.__states__ = ['ca','az','nv','tx'] #defaults to ['ca','az','nv','tx']
         self.__defaultBackoff__ = 3 #may not work with values under 3. This is the minimum number of seconds the program waits between clicks on webpages.
         self.__variableBackoff__ = 2 #how random the interval between web page clicks are. "0" means it clicks every $defaultBackoff (default 3) seconds. "2" means it clicks between $defaultBackoff (default 3) seconds and $defaultBackoff (default 5) seconds.
         self.__sheetSaveName__ = "99 Cent Store Locations" #The Sheet Save Name
